473a0471b31aa0b17042062361f340eb27f92ea6
Jérémy <jeremy.perrouault@ascadis.fr>
2026-05-05 16:33:39 +0000

    JOUR 1 - COMMANDES JAVA

---
+++ UNIX/java.sh
@@ -0,0 +1,14 @@
+# Pour compiler une classe Java
+javac NomFichierApp.java
+
+# Pour exécuter une classe compilée
+java NomFichierApp
+
+# Pour compiler et exécuter un fichier java à la volée
+java NomFichierApp.java
+
+# Pour consulter le bycode généré dans le fichier .class
+javap -c NomFichierApp
+
+# Pour accéder au shell Java
+jshell
