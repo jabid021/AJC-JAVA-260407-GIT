6103195d672388adb24b62ed23948d168600ad65
Jérémy <jeremy.perrouault@gmail.com>
2026-05-11 13:08:55 +0000

    JOUR 2 - CMD vs ENTRYPOINT

--- DOCKER/dockerfile/ajc-java/Application.java
+++ DOCKER/dockerfile/ajc-java/Application.java
@@ -2,5 +2,9 @@
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        for (String s : args) {
+            System.out.println(s);
+        }
     }
 }
