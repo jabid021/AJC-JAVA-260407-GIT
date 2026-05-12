f8f9faaf40adbe472ded86513a0ef8d80658ade5
Jérémy <jeremy.perrouault@gmail.com>
2026-05-12 07:58:20 +0000

    JOUR 3 - COMMANDES COMPOSE

---
+++ DOCKER/commandes/compose.sh
@@ -0,0 +1,20 @@
+# Démarrer un Docker Compose
+docker compose up
+
+# Démarrer un Docker Compose avec le mode détaché
+docker compose up -d
+
+# Supprimer les containers
+docker compose down
+
+# Supprimer les containers (avec les volumes)
+docker compose down -v
+
+# Arrêter tous les containers
+docker compose stop
+
+# Démarrer tous les containers
+docker compose start
+
+# Rédémarrer tous les containers
+docker compose restart
