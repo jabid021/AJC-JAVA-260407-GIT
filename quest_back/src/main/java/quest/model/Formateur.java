a3e081af9972ef59d2f71323e889680b96a837bc
GILET Ronan <ronan.gilet@gmail.com>
2026-05-18 12:18:37 +0200

    Personne + Stagiaire + Formateur Mapped

--- quest_back/src/main/java/quest/model/Formateur.java
+++ quest_back/src/main/java/quest/model/Formateur.java
@@ -2,14 +2,25 @@ package quest.model;
 
 import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
+@DiscriminatorValue("Formateur")
 public class Formateur extends Personne  {
+	@Column(nullable=true)
 	private boolean admin;
+	@OneToMany(mappedBy="formateur")
 	private List<Module> affectations;
 	
 	public Formateur(Integer id, String login, String password, String nom, String prenom, Genre civilite, boolean admin) {
 		super(id, login, password, nom, prenom, civilite);
 		this.admin = admin;
 	}
+	
+	public Formateur() {}
 
 	public boolean isAdmin() {
 		return admin;
