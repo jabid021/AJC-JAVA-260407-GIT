f9d20b41972fe861bee0c677d3244073789c1b0d
Hestia <clea.vcd@gmail.com>
2026-05-18 12:43:01 +0200

    maj filiere

--- quest_back/src/main/java/quest/model/Filiere.java
+++ quest_back/src/main/java/quest/model/Filiere.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 
 @Entity
@@ -20,8 +21,8 @@ public class Filiere {
 	private String libelle;  
 	private LocalDate debut; 
 	private LocalDate fin;
-	@OneToMany
-	@JoinColumn(nullable=false)
+	@ManyToOne
+	@JoinColumn(name="salle",nullable=false)
 	private Salle salle; 
 	@OneToMany(mappedBy="filiere")
 	private List<Stagiaire> stagiaires;
