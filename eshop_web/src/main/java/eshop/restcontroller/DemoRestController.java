d21c78a3bc6374a12732cdfb9d2321cb1e739374
Jordan <Jordanabid@gmail.com>
2026-06-17 13:01:45 +0200

    Spring MVC - Jour 3 - @PostMapping

---
+++ eshop_web/src/main/java/eshop/restcontroller/DemoRestController.java
@@ -0,0 +1,33 @@
+package eshop.restcontroller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import eshop.model.Produit;
+
+@RestController
+@RequestMapping("/api/demo")
+public class DemoRestController {
+
+	@GetMapping("/{nb}")
+	public String demo(@PathVariable(name="nb") Integer nombre, @RequestParam(name="log",required = false) String login) 
+	{
+		return "Requête avec les valeurs "+nombre+" et "+login;
+	}
+	
+	
+	@GetMapping
+	public Produit produit() 
+	{
+		return new Produit();
+	}
+	
+	
+}
