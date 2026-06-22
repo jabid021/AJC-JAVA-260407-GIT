c875b9808be44e7ed895ce965f16226a524b4881
Jérémy <jeremy.perrouault@gmail.com>
2026-06-22 08:50:57 +0000

    JOUR 2 - FILTER AUTH

--- BOOT/formation-spring-boot-security/src/main/java/fr/formation/security/DemoFilter.java
+++ BOOT/formation-spring-boot-security/src/main/java/fr/formation/security/DemoFilter.java
@@ -1,7 +1,14 @@
 package fr.formation.security;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,6 +23,17 @@ public class DemoFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         System.out.println("JE SUIS PASSE DANS LE FILTRE");
+
+        // Création de la liste des autorisations
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+
+        // Pour créer une authentification à la mano, utiliser la classe UsernamePasswordAuthenticationToken
+        Authentication auth = new UsernamePasswordAuthenticationToken("LeUser", null, authorities);
+
+        // On va donner cette authentification au contexte de sécurité
+        SecurityContextHolder.getContext().setAuthentication(auth);
 
         // Sans cette instruction, on ne passe pas au filtre suivant
         filterChain.doFilter(request, response);
