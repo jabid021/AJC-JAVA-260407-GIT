6cf3be162a0d749ed0b0167f710673305628f1f9
Jérémy <jeremy.perrouault@gmail.com>
2026-06-23 10:49:53 +0000

    JOUR 1 - TDD ADD STRING

--- TDD/formation-tdd/src/main/java/fr/formation/Calculatrice.java
+++ TDD/formation-tdd/src/main/java/fr/formation/Calculatrice.java
@@ -1,6 +1,9 @@
 package fr.formation;
 
+import java.util.stream.Stream;
+
 import fr.formation.exception.CantDivideByZeroException;
+import fr.formation.exception.NegativeNotAllowedException;
 
 public class Calculatrice {
     public int add(int a, int b) {
@@ -11,6 +14,40 @@ public class Calculatrice {
         // return 11;
 
         return a + b;
+    }
+
+    public int add(String value) {
+        if (value == null || value.isEmpty()) {
+            return 0;
+        }
+
+        // String[] intStrings = value.split("[,;\\n]+");
+        // int total = 0;
+
+        // for (String intString : intStrings) {
+        //     int intVal = Integer.parseInt(intString);
+
+        //     if (intVal < 0) {
+        //         throw new NegativeNotAllowedException();
+        //     }
+
+        //     total += intVal;
+        // }
+
+        // return total;
+
+        return Stream.of(value.split("[,;\\n]+"))
+            .mapToInt(val -> {
+                int intVal = Integer.parseInt(val);
+
+                if (intVal < 0) {
+                    throw new NegativeNotAllowedException();
+                }
+
+                return intVal;
+            })
+            .sum()
+        ;
     }
 
     public float divide(int a, int b) {
--- TDD/formation-tdd/src/test/java/fr/formation/CalculatriceTest.java
+++ TDD/formation-tdd/src/test/java/fr/formation/CalculatriceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import fr.formation.exception.CantDivideByZeroException;
+import fr.formation.exception.NegativeNotAllowedException;
 
 public class CalculatriceTest {
     private Calculatrice calc;
@@ -71,6 +72,163 @@ public class CalculatriceTest {
         Assertions.assertThrows(
             CantDivideByZeroException.class,
             () -> this.calc.divide(a, b)
+        );
+    }
+
+    @Test
+    void shouldAddReturn0WhenNull() {
+        // given
+        String value = null;
+
+        // when
+        int result = this.calc.add(value);
+
+        // then
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    void shouldAddReturn0WhenEmpty() {
+        // given
+        String value = "";
+
+        // when
+        int result = this.calc.add(value);
+
+        // then
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    void shouldAddReturn2When2() {
+        // given
+        String value = "2";
+
+        // when
+        int result = this.calc.add(value);
+
+        // then
+        Assertions.assertEquals(2, result);
+    }
+
+    @Test
+    void shouldAddReturn3When3() {
+        // given
+        String value = "3";
+
+        // when
+        int result = this.calc.add(value);
+
+        // then
+        Assertions.assertEquals(3, result);
+    }
+
+    @Test
+    void shouldAddReturn36When12And5And19() {
+        // given
+        String value = "12,5,19";
+
+        // when
+        int result = this.calc.add(value);
+
+        // then
+        Assertions.assertEquals(36, result);
+    }
+
+    @Test
+    void shouldAddReturn30When12And18() {
+        // given
+        String value = "12,18";
+
+        // when
+        int result = this.calc.add(value);
+
+        // then
+        Assertions.assertEquals(30, result);
+    }
+
+    @Test
+    void shouldAddThrowNegativeNotAllowedException() {
+        // given
+        String value = "10,-5,8";
+
+        // when & then
+        Assertions.assertThrows(
+            NegativeNotAllowedException.class,
+            () -> this.calc.add(value)
+        );
+    }
+
+    @Test
+    void shouldAddReturn20When8And12SemiColon() {
+        // given
+        String value = "8;12";
+
+        // when
+        int result = this.calc.add(value);
+
+        // then
+        Assertions.assertEquals(20, result);
+    }
+
+    @Test
+    void shouldAddReturn36When12And5And19NewLine() {
+        // given
+        String value = "12\n5\n19";
+
+        // when
+        int result = this.calc.add(value);
+
+        // then
+        Assertions.assertEquals(36, result);
+    }
+
+    @Test
+    void shouldAddReturn56When12And5And19Mixin() {
+        // given
+        String value = "12,5;19\n20";
+
+        // when
+        int result = this.calc.add(value);
+
+        // then
+        Assertions.assertEquals(56, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+        "null:0",
+        "'':0",
+        "2:2",
+        "15:15",
+        "12,5,19:36",
+        "12;5;19:36",
+        "'12\n5\n19':36"
+    }, delimiter = ':', nullValues = "null")
+    void shouldAddStringReturnOk(String value, int expected) {
+        // given
+
+        // when
+        int result = this.calc.add(value);
+
+        // then
+        Assertions.assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+        "-2",
+        "15,-15",
+        "12;-5;19",
+        "'12\n-5\n19'"
+    }, delimiter = ':')
+    void shouldAddStringThrowNegativeNotAllowedException(String value) {
+        // given
+
+        // when & then
+        Assertions.assertThrows(
+            NegativeNotAllowedException.class,
+            () -> this.calc.add(value)
         );
     }
 }
