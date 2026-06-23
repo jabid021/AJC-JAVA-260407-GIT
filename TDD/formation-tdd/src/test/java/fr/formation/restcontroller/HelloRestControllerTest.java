b448aceae1361c5ffceec714cb42fb5b836c8071
Jérémy <jeremy.perrouault@gmail.com>
2026-06-23 14:38:04 +0000

    JOUR 1 - TEST WEB MVC AVEC MOCK

--- TDD/formation-tdd/src/test/java/fr/formation/restcontroller/HelloRestControllerTest.java
+++ TDD/formation-tdd/src/test/java/fr/formation/restcontroller/HelloRestControllerTest.java
@@ -1,20 +1,29 @@
 package fr.formation.restcontroller;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
-@AutoConfigureMockMvc // Permet d'injecter un MockMvc dans le contexte de Spring
+import fr.formation.repo.HelloRepository;
+
+// @SpringBootTest
+// @AutoConfigureMockMvc // Permet d'injecter un MockMvc dans le contexte de Spring
+
+// Avec WebMvcTest, le MockMvc est injecté par défaut
+@WebMvcTest(controllers = HelloRestController.class)
 public class HelloRestControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @MockitoBean // On précise à Spring Test que le HelloRepository est simulé par Mockito
+    private HelloRepository repository;
 
     @Test
     @WithMockUser // Permet de simuler un utilisateur connecté
@@ -33,5 +42,8 @@ public class HelloRestControllerTest {
 
         // On vérifie que le contenu JSON existe bien
         result.andExpect(MockMvcResultMatchers.jsonPath("$").exists());
+
+        // On vérifie si on a appelé UNE FOIS ET UNE SEULE FOIS le findAll
+        Mockito.verify(this.repository).findAll();
     }
 }
