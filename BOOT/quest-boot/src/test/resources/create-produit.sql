53f114fd9ae71239bdec1abff40b379f5fcfa2e3
Cécile <ccilejouanne@yahoo.fr>
2026-06-24 15:34:24 +0200

    j'ai oublié un truc

---
+++ BOOT/quest-boot/src/test/resources/create-produit.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO produit (nom, prix) VALUES
+    ('Parachute de France', 4999),
+    ('Casque de moto', 499),
+    ('Enceinte portative', 1500),
+    ('Ordinateur qui chauffe pas', 15000)
+;
