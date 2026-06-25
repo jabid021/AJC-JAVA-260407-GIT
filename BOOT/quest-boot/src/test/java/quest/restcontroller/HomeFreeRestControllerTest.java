dd6deb34bf2ede2c31d2b3c0cd5deb8ba6c4621c
rathieux <rathieux@gmail.com>
2026-06-25 16:26:37 +0200

    test controller

---
+++ BOOT/quest-boot/src/test/java/quest/restcontroller/HomeFreeRestControllerTest.java
@@ -0,0 +1,24 @@
+package quest.restcontroller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(HomeFreeRestController.class)
+class HomeFreeRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void free_shouldReturnMessage() throws Exception {
+        mockMvc.perform(get("/api/home/free"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("FREEEE Mathieu !!"));
+    }
+}
