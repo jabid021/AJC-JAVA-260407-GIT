f9a0267c886105be2ee4e6f49f90a988870891f5
Jérémy <jeremy.perrouault@gmail.com>
2026-06-26 14:39:46 +0000

    JOUR 1 - CORRECTION TODO LIST

--- ANGULAR/formation-angular/src/app/app.ts
+++ ANGULAR/formation-angular/src/app/app.ts
@@ -13,11 +13,22 @@ import { Todo } from './model/todo';
 export class App {
   protected title: string = 'Formation Angular AJC';
   protected couleur: string = "black";
-  protected todo: Todo = { id: 1, title: "Le titre", completed: false };
+  // protected todo: Todo = { id: 1, title: "Le titre", completed: false };
   protected demoBoolean: boolean = false;
   protected demoFor: string[] = [ "Chaine A", "Chaine B", "Chaine C", "Chaine D" ];
 
+  // Correction exercice Todo List
+  protected formTodo: Todo = { id: 0, title: "", completed: false };
+  // protected todos: Array<Todo> = []
+  protected todos: Todo[] = [ ];
+
   public changeTitle() {
     this.title = "Le nouveau titre";
+  }
+
+  public ajouterTodo() {
+    this.todos.push(this.formTodo);
+
+    this.formTodo = { id: 0, title: "", completed: false };
   }
 }
