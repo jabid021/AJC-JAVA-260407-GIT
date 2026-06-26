eeef779857899434466deee7de5e043204eec534
Jérémy <jeremy.perrouault@gmail.com>
2026-06-26 15:22:00 +0000

    JOUR 1 - INIT COMPONENT (ONINIT & ONDESTROY)

--- ANGULAR/formation-angular/src/app/component/premier-composant/premier-composant.ts
+++ ANGULAR/formation-angular/src/app/component/premier-composant/premier-composant.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, Input } from '@angular/core';
+import { Component, HostListener, Input, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-premier-composant',
@@ -6,12 +6,22 @@ import { Component, HostListener, Input } from '@angular/core';
   templateUrl: './premier-composant.html',
   styleUrl: './premier-composant.css',
 })
-export class PremierComposant {
+export class PremierComposant implements OnInit, OnDestroy {
   @Input('prenom')
   public prenom: string = "Zoro";
 
   @HostListener('click')
   public onClick() {
     alert('ok');
+  }
+
+  // Méthode qui va être déclenchée par Angular au moment où le composant sera initialisé !
+  ngOnInit(): void {
+    console.log("Initialisation du composant !");
+  }
+
+  // Méthode qui va être déclenchée par Angular au moment où le composant sera détruit !
+  ngOnDestroy(): void {
+    console.log("Destruction du composant !")
   }
 }
