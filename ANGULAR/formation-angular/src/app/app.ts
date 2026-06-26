244610873a6984a92370c38b968e40ebb11b6cc0
Jérémy <jeremy.perrouault@gmail.com>
2026-06-26 13:04:31 +0000

    JOUR 1 - INIT NGIF / NGFOR

--- ANGULAR/formation-angular/src/app/app.ts
+++ ANGULAR/formation-angular/src/app/app.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Todo } from './model/todo';
 
 
 @Component({
@@ -12,6 +13,9 @@ import { FormsModule } from '@angular/forms';
 export class App {
   protected title: string = 'Formation Angular AJC';
   protected couleur: string = "black";
+  protected todo: Todo = { id: 1, title: "Le titre", completed: false };
+  protected demoBoolean: boolean = false;
+  protected demoFor: string[] = [ "Chaine A", "Chaine B", "Chaine C", "Chaine D" ];
 
   public changeTitle() {
     this.title = "Le nouveau titre";
