fcabe6ef7a97ec5dc364f0e837e48805aafbe4d0
Jérémy <jeremy.perrouault@gmail.com>
2026-06-26 12:36:59 +0000

    JOUR 1 - CORRECTION COULEURS

--- ANGULAR/formation-angular/src/app/app.ts
+++ ANGULAR/formation-angular/src/app/app.ts
@@ -1,15 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
-  imports: [ FormsModule ],
+  imports: [ FormsModule, CommonModule ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title: string = 'Formation Angular AJC';
+  protected couleur: string = "black";
 
   public changeTitle() {
     this.title = "Le nouveau titre";
