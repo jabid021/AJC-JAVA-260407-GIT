fd169d02fedb16d4231d774d9d31949116819ddd
Jérémy <jeremy.perrouault@gmail.com>
2026-06-26 08:35:15 +0000

    JOUR 1 - COMMANDES NPM

---
+++ ANGULAR/commandes/npm.sh
@@ -0,0 +1,14 @@
+# Voir la version
+npm -v
+
+# Installer une nouvelle dépendance sur le projet (dans le répertoire du projet, à la racine)
+npm install nom_dependance
+npm i nom_dependance
+
+# Installer les dépendances manquantes au projet (dans le répertoire du projet)
+npm install
+npm i
+
+# Installer un script / application de manière globale, sur l'ordinateur
+npm install -g nom_app
+npm i -g nom_app
