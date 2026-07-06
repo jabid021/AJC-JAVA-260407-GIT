2ac18f612a3d648aa9ddcdd950c94fea9ed9fd11
Jérémy <jeremy.perrouault@gmail.com>
2026-07-06 07:44:08 +0000

    JOUR 2 - CORRECTION IMPL JPA

--- QUARKUS/formation-quarkus/src/main/java/fr/formation/api/ProduitResource.java
+++ QUARKUS/formation-quarkus/src/main/java/fr/formation/api/ProduitResource.java
@@ -1,14 +1,15 @@
 package fr.formation.api;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 
 import fr.formation.model.Produit;
+import fr.formation.repo.ProduitRepository;
 import fr.formation.request.CreateOrUpdateProduitRequest;
 import fr.formation.response.ProduitResponse;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
@@ -16,17 +17,15 @@ import jakarta.ws.rs.PathParam;
 
 @Path("/api/produit")
 public class ProduitResource {
-    private List<Produit> produits = new ArrayList<>();
-    private int lastId = 2;
+    private final ProduitRepository repository;
 
-    public ProduitResource() {
-        produits.add(new Produit(1, "Parachute", new BigDecimal("9850")));
-        produits.add(new Produit(2, "Casque", new BigDecimal("499")));
+    public ProduitResource(ProduitRepository repository) {
+        this.repository = repository;
     }
 
     @GET
     public List<ProduitResponse> findAll() {
-        return this.produits
+        return this.repository.findAll()
             .stream()
             .map(ProduitResponse::convert)
             .toList()
@@ -34,33 +33,36 @@ public class ProduitResource {
     }
 
     @POST
+    @Transactional
     public int create(CreateOrUpdateProduitRequest request) {
         Produit produit = new Produit();
 
         produit.setLibelle(request.getLibelle());
         produit.setPrix(request.getPrix());
 
-        produit.setId(++this.lastId);
+        this.repository.persist(produit);
 
-        this.produits.add(produit);
-
-        return this.lastId;
+        return produit.getId();
     }
 
     @PUT
     @Path("/{id}")
+    @Transactional
     public int update(@PathParam("id") Integer id, CreateOrUpdateProduitRequest request) {
-        Produit produit = this.produits.stream().filter(p -> p.getId() == id).findFirst().orElseThrow();
+        Produit produit = this.repository.findByIdOptional(id).orElseThrow(NotFoundException::new);
 
         produit.setLibelle(request.getLibelle());
         produit.setPrix(request.getPrix());
+
+        this.repository.persist(produit);
 
         return id;
     }
 
     @DELETE
     @Path("/{id}")
+    @Transactional
     public void deleteById(@PathParam("id") Integer id) {
-        this.produits.removeIf(p -> p.getId() == id);
+        this.repository.deleteById(id);
     }
 }
